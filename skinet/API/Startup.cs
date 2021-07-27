ab26b7d4d0add0f77eeafb3bd55d233c41253ff9
halilkosee <51232723+halilkosee@users.noreply.github.com>
2021-07-27 18:08:28 +0300

    CORS added

--- skinet/API/Startup.cs
+++ skinet/API/Startup.cs
@@ -26,6 +26,13 @@ namespace API
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();         
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
           
         }
 
@@ -41,6 +48,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
